6099f8a74f83c7df9836e0a84605c39283861ed7
Semir MALICKY <arkaentech@gmail.com>
2025-05-30 15:23:02 +0200

    saison 2 test.py

--- test.py
+++ test.py
@@ -8,3 +8,12 @@ liste=[1,2,3,4]
  echo "Hello World episode 7"
  echo "Hello World episode 8"
  
+ echo "Hello World season 2"
+ echo "Hello World episode 2"
+ echo "Hello World episode 3"
+ echo "Hello World episode 4"
+ echo "Hello World episode 5"
+ echo "Hello World episode 6"
+ echo "Hello World episode 7"
+ echo "Hello World episode 8"
+ 
